b7c47da471d904e8d4eaf3a4a2ba3fa7a62002a8
Saurabh Daware <saurabhdaware2001@gmail.com>
2020-08-29 17:16:24 +0530

    Adding Hello.c

---
+++ Hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+
+
+int main(void){
+    char name[15];
+    printf("Enter your name\n");
+    scanf("%s",name);
+    time_t now;
+    struct tm * timeinfo;
+    time (&now);
+    timeinfo = localtime(&now);
+    int timeOfDay = timeinfo->tm_hour;
+
+    if(timeOfDay >= 06 && timeOfDay < 12){printf("Good Morning, %s!!",name);}
+    else if(timeOfDay >= 12 && timeOfDay < 17){printf("Good Afternoon, %s!!",name);}
+    else if(timeOfDay >= 17 && timeOfDay < 22){printf("Good Morning, %s!!",name);}
+    else {printf("Good Night, %s!!",name);}
+}
