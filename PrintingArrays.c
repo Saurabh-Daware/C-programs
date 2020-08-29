fa5277be6aa7a1e8050ad1ad5e3c1bcfebd8ce3a
Saurabh Daware <saurabhdaware2001@gmail.com>
2020-08-29 17:16:48 +0530

    Adding PrintingArray.c

---
+++ PrintingArrays.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void){
+   int n;
+   int arr[20];
+   int *ptr = arr;
+   
+    printf("Enter array length: ");
+    scanf("%d",&n);                                             //asking of length of array
+   for(int i = 0; i < n; i++){
+       printf("Enter element no.%d: ",i+1);                     //taking array from user
+       scanf("%d",ptr+i);
+   }
+   printf("\n");
+   printf("=============================================\n");
+   printf("your array is: \n");
+   for(int i = 0; i < n; i++){
+       printf(" %d ",arr[i]);                                   //printing array
+   }
+   printf("\n");
+    printf("=============================================\n");
+}
